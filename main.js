aed37bdf7d72cf6910349c211a5e37b649270949
MicFin <mrfinneran@gmail.com>
2019-06-16 11:42:27 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const getPrimes = function(rangeLimit) {
+  // Create your range, starting at two and ending at the given limit.
+
+  // The algorithm consists of repeating the following over and over:
+
+    // take the next available unmarked number in your list (it is prime)
+    // remove all the multiples of that number (they are not prime)
+    // Repeat until you don't have any possible primes left in your range.
+
+  // When the algorithm terminates, all the numbers in the list that have not been removed are prime.
+  // loop through all the prime numbers and print them to the console
+}
+
+getPrimes();
